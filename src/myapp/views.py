a5066248bf1f82d58cdfd9747d4ba7e4f54181a2
DmitriyAnishchenko <1348010@gmail.com>
2019-05-14 22:10:26 +0300

    add index2

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
-   if request.method == 'POST':
 
-      name = request.POST.get('name')
-      return HttpResponse('len is {}' .format(len(name)))
-   return HttpResponse('It is Get request')
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        return HttpResponse('len is {}'.format(len(name)))
+    return HttpResponse('It is Get request')
+
+
+def index2(request):
+    age = request.POST.get('age')
+    age = int(age)
+    if age < 18:
+        return HttpResponse('В доступе отказано')
+    return HttpResponse('Добро пожаловать')
