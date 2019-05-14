0ab6417385e89c21821b0ba0a32cb4ea100f9983
DmitriyAnishchenko <1348010@gmail.com>
2019-05-14 21:44:27 +0300

    HTMl

--- src/myapp/views.py
+++ src/myapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-   return HttpResponse("Hello Dima")
+   if request.method == 'POST':
+
+      name = request.POST.get('name')
+      return HttpResponse('len is {}' .format(len(name)))
+   return HttpResponse('It is Get request')
