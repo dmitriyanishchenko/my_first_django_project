0af00907052778c69b20455e76d258069087c03a
dmitriyanishchenko <1348010@gmail.com>
2019-05-18 19:55:56 +0300

    add path in cw_18.urls

--- src/MyProject1/urls.py
+++ src/MyProject1/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('index/', index),
     path('index2/', index2),
     path('cw_18_1/', include('cw_18.urls')),
-    path('render_name/', render_name),
+    path('render_name/', include('cw_18.urls')),
 
 ]
