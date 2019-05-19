6c6a4167909ba151b370055716b7bd5c308d7a0a
dmitriyanishchenko <1348010@gmail.com>
2019-05-19 13:10:06 +0300

    create view task 18_2

--- src/hw_18/urls.py
+++ src/hw_18/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+
+from .views import line_to_file
+
+
+urlpatterns = [
+
+    path('line_to_file/', line_to_file),
+]
--- src/hw_18/views.py
+++ src/hw_18/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-# Create your views here.
+def line_to_file(request):
+    """
+    Создать форму из трех полей: имя, фамилия, возраст.
+    Создать представление (view. В случае GET запроса отображать форму.
+    В случае POST запроса записывать все три поля в виде одной строки в файл
+    и после  отображать форму
+
+    """
+    if request.method == "POST":
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        age = request.POST.get('age')
+        result_string = f' Name is {name}, surname is {surname}, age is {age}'
+        with open('test.txt', 'w') as my_file:
+            my_file.write('result_string')
+            my_file.close()
+        return HttpResponse(result_string)
+
+
+
