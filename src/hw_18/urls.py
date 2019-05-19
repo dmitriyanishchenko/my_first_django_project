7feeb9cc5351f61992f2bfcdf159a6212b0d90ef
dmitriyanishchenko <1348010@gmail.com>
2019-05-19 18:55:25 +0300

    task_18_3 is ready

--- src/hw_18/urls.py
+++ src/hw_18/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import line_to_file
+from .views import line_to_file, file_to_list
 
 
 urlpatterns = [
 
     path('line_to_file/', line_to_file),
+    path('file_to_list/', file_to_list),
 ]
--- src/hw_18/views.py
+++ src/hw_18/views.py
@@ -18,9 +18,29 @@ def line_to_file(request):
         name = request.POST.get('name')
         surname = request.POST.get('surname')
         age = request.POST.get('age')
-        result_string = f'{name} {surname} {age}'
-        with open('test_18_2.json', 'w') as my_file:
+        result_string = f'{name},{surname},{age}'
+        with open('test_18_2.txt', 'w') as my_file:
             my_file.write(result_string + '\n')
-            my_file.close()
+        my_file.close()
         template = loader.get_template('form_line.html')
         return HttpResponse(template.render({}, request))
+
+
+def file_to_list(request):
+    """
+    Создать форму, принимающую на вход список и отображающая его.
+    Создать представление. В случае GET запроса считать все строки файла в список.
+    Произвести рендиринг шаблона и данных (списка строк) и после вернуть пользователю.
+
+    """
+    if request.method == "GET":
+        with open('test_18_2.txt', 'r') as my_file:
+            file_string = my_file.readline()
+        res_split = file_string.split(',')
+        name = res_split[0]
+        surname = res_split[1]
+        age = res_split[2]
+        template = loader.get_template('display_line.html')
+        return HttpResponse(template.render({'name': name, 'surname': surname, 'age': age}, request))
+
+
