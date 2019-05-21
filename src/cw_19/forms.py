3437f8b40259dd160fdd02da61cd6a5b4023a3a2
DmitriyAnishchenko <1348010@gmail.com>
2019-05-21 21:56:48 +0300

    add widget textarea

--- src/cw_19/forms.py
+++ src/cw_19/forms.py
@@ -5,5 +5,5 @@ class PostForm(forms.Form):
    firstname = forms.CharField(max_length=20)
    lastname = forms.CharField(max_length=20)
    age = forms.IntegerField(min_value=0, max_value=99)
-   comment = forms.CharField(max_length=300)
+   comment = forms.CharField(max_length=300, widget=forms.Textarea)
 
