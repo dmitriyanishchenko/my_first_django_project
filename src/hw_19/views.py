fad13bc4a4ff249e115d7ea416db6988a32a9d92
dmitriyanishchenko <1348010@gmail.com>
2019-05-22 21:49:24 +0300

    add comment in view

--- src/hw_19/views.py
+++ src/hw_19/views.py
@@ -5,6 +5,15 @@ from django.template import loader
 
 
 def air_ticket(request):
+    """
+    Содать форму через django forms, описывающую форму заказа авиабилета.
+    форма должна содержать следующие поля - имя, откуда, куда, сколько человек, дата.
+    При отправке данных пользователем проверять валидность данных, если они валидны и
+    количество человек равно 1, то вывести результат - "стоимость 100 $", если количество
+    человек больше 1, то стоимость должна считаться по формуле 100*2*количество человек.
+    Если данные не валидны, то вывести ошибку.
+
+    """
     if request.method == 'GET':
         form = PostForm()
         context = {'form': form}
@@ -13,19 +22,16 @@ def air_ticket(request):
         form = PostForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-
             name = data.get('name')
             city_from = data.get('city_from')
             city_to = data.get('city_to')
             number = data.get('number')
             date = data.get('date')
-
             if number == 1:
                 cost = 100
                 template = loader.get_template('display1_line.html')
                 return HttpResponse(template.render({'name': name, 'city_from': city_from, 'number': number,
                                                      'city_to': city_to, 'date': date, 'cost': cost}, request))
-
             elif number > 1:
                 cost = number * 2 * 100
                 template = loader.get_template('display1_line.html')
