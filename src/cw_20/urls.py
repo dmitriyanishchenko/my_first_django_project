eef43841415a62b5e4d414b7f1db4528483de495
DmitriyAnishchenko <1348010@gmail.com>
2019-05-23 21:54:45 +0300

    cw_20 end

--- src/cw_20/urls.py
+++ src/cw_20/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from cw_20.views import home
+
 
 
 urlpatterns = [
+    path('home/', home, name='home')
 ]
--- src/cw_20/views.py
+++ src/cw_20/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+def home(reguest):
 
 # Create your views here.
+ome
