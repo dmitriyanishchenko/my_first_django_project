0bb467a559c9d58e051f019adc5bdf03e7e6defc
DmitriyAnishchenko <1348010@gmail.com>
2019-05-23 21:01:02 +0300

    cw_20 pip install

--- src/cw_20/models.py
+++ src/cw_20/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Customer(models.Model):
+   firstname = models.CharField(max_length=255)
+   lastname = models.CharField(max_length=255)
+   age = models.IntegerField()
+   profession = models.CharField(max_length=255)
+
+
 # Create your models here.
