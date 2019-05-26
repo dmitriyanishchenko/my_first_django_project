04960af85ce271a6ba0154434493d36fb53c69df
dmitriyanishchenko <1348010@gmail.com>
2019-05-26 17:37:14 +0300

    #cw_20 finish

--- src/cw_20/urls.py
+++ src/cw_20/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from cw_20.views import home
-
+from .views import (
+    home,
+    create_customer,
+    edit_customer,
+    remove_customer,
+)
 
 urlpatterns = [
-    path('home/', home, name='home')
+    path('home/', home, name='home'),
+    path('add/', create_customer, name='create_customer'),
+    path('edit/<int:customer_id>', edit_customer, name='edit_customer'),
+    path('remove/<int:customer_id>', remove_customer, name='remove_customer'),
 ]
--- src/cw_20/views.py
+++ src/cw_20/views.py
@@ -1,8 +1,77 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import (
+    render,
+    redirect,
+)
+from .models import Customer
+from .forms import CustomerForm
 
 
 def home(request):
-    return render(request, 'home.html')
+    customers = Customer.objects.all()
+    context = {'customers': customers}
+    return render(request, 'home.html', context)
 
 
-# Create your views here.
+def create_customer(request):
+    if request.method == 'GET':
+        context = {'form': CustomerForm()}
+        return render(request, 'add.html', context)
+    elif request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            firstname = data.get('firstname')
+            lastname = data.get('lastname')
+            age = data.get('age')
+            profession = data.get('profession')
+            Customer.objects.create(
+                firstname=firstname,
+                lastname=lastname,
+                age=age,
+                profession=profession,
+            )
+            return redirect('home')
+        else:
+            errors = form.errors
+            return HttpResponse(f'{errors}')
+    return HttpResponse('Wrong request method')
+
+
+def edit_customer(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    if request.method == 'GET':
+        context = {
+            'customer_id': customer_id,
+            'form': CustomerForm(
+                initial={
+                    'firstname': customer.firstname,
+                    'lastname': customer.lastname,
+                    'age': customer.age,
+                    'profession': customer.profession,
+                },
+            ),
+        }
+        return render(request, 'edit.html', context)
+    elif request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Customer.objects.filter(id=customer_id).update(
+                firstname=data.get('firstname'),
+                lastname=data.get('lastname'),
+                age=data.get('age'),
+                profession=data.get('profession'),
+            )
+            return redirect('home')
+        else:
+            errors = form.errors
+            return HttpResponse(f'{errors}')
+    return HttpResponse('Wrong request method')
+
+
+def remove_customer(request, customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    print(f'{customer.firstname} has been removed')
+    customer.delete()
+    return redirect('home')
