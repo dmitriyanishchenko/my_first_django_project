128a3536ef298c28e7ecd4899b7810af00973d32
dmitriyanishchenko <1348010@gmail.com>
2019-05-27 21:36:05 +0300

    #hw20 edit templates and forms

--- src/hw_20/forms.py
+++ src/hw_20/forms.py
@@ -5,6 +5,6 @@ class CarForm(forms.Form):
     brand = forms.CharField(max_length=255)
     model = forms.CharField(max_length=255)
     color = forms.CharField(max_length=255)
-    weight = forms.IntegerField()
+    weight = forms.IntegerField(min_value=700)
     fullname = forms.CharField(max_length=255)
     year = forms.CharField(max_length=255)
