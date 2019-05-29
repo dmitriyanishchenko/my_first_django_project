8ed7be71c2961aef156c5806e22d83ccfbb9781b
DmitriyAnishchenko <1348010@gmail.com>
2019-05-30 00:13:56 +0300

    #cw_21 add class Hobbie

--- src/cw_21/admin.py
+++ src/cw_21/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from cw_21.models import ContactInfo, Person, Group
+from cw_21.models import ContactInfo, Person, Group, Hobbie
 
 admin.site.register(ContactInfo)
 admin.site.register(Person)
 admin.site.register(Group)
+admin.site.register(Hobbie)
 
 
 # Register your models here.
--- src/cw_21/models.py
+++ src/cw_21/models.py
@@ -1,28 +1,30 @@
 from django.db import models
 
-class ContactInfo(models.Model):
-   phone = models.CharField(max_length=50, null=True, default=None)
-   address = models.CharField(max_length=50, null=True, default=None)
 
-   def __str__(self):
-      return f"{self.phone} {self.address}"
+class ContactInfo(models.Model):
+    phone = models.CharField(max_length=50, null=True, default=None)
+    address = models.CharField(max_length=50, null=True, default=None)
+
+    def __str__(self):
+        return f"{self.phone} {self.address}"
+
 
 class Person(models.Model):
-   firstname = models.CharField(max_length=255, default=None)
-   lastname = models.CharField(max_length=255, default=None)
-   age = models.IntegerField()
-   profession = models.CharField(max_length=255, default=None)
-   contact_info = models.OneToOneField(
-       ContactInfo, null=True,
-       related_name='person',
-       on_delete=models.SET_NULL,
-   )
-   group = models.ForeignKey('Group', null=True, on_delete=models.SET_NULL, related_name='person')
+    firstname = models.CharField(max_length=255, default=None)
+    lastname = models.CharField(max_length=255, default=None)
+    age = models.IntegerField()
+    profession = models.CharField(max_length=255, default=None)
+    contact_info = models.OneToOneField(
+        ContactInfo, null=True,
+        related_name='person',
+        on_delete=models.SET_NULL,
+    )
+    group = models.ForeignKey('Group', null=True, on_delete=models.SET_NULL, related_name='person')
+    hobbie = models.ManyToManyField('Hobbie', related_name='person')
 
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} {self.age} {self.profession} {self.contact_info}"
 
-
-   def __str__(self):
-      return f"{self.firstname} {self.lastname} {self.age} {self.profession} {self.contact_info}"
 
 class Group(models.Model):
     serial_number = models.CharField(max_length=4, default=None)
@@ -31,6 +33,13 @@ class Group(models.Model):
     finish_date = models.DateField(null=False)
 
     def __str__(self):
-        return f'{self.serial_number} {self.size}'
+        return f'{self.serial_number}'
+
+
+class Hobbie(models.Model):
+    name = models.CharField(max_length=255, default=None)
+
+    def __str__(self):
+        return f'{self.name}'
 
 # Create your models here.
