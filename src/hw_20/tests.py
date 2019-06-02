5ab79b5127dd4ea2870a8ae4d5e9f4f112a60c41
DmitriyAnishchenko <1348010@gmail.com>
2019-06-02 18:16:39 +0300

    #hw_20 add test CarViews

--- src/hw_20/tests.py
+++ src/hw_20/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
+class TestCarViews(TestCase):
+    def test_home(self):
+        result = self.client.get('/hw_20/')
+        self.assertEqual(result.status_code, 200)
 # Create your tests here.
